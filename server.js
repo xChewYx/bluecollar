f29179f1956d6c979cc0c136d22a59cf83b88904
Scott Irvin <sirvin85@gmail.com>
2014-11-18 15:16:05 -0500

    heroku deploy issues

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-console.log("hello")
 var statik = require('statik');
-console.log("poop")
-console.log(statik)
 statik({
-    port: 3000,
+    port: process.env.PORT || 1337,
     root: "./public"
 });
